20eeb3b6352fd65055c439ba6c5d5519bd03d08b
Abhishek Kaushik <akaushik079@gmail.com>
2024-10-11 12:42:58 +0530

    Leetcode: add solution for Customer Who Visited but Did Not Make Any Transactions

---
+++ leetcode/customer_who_visited_but_did_not_transact.sql
@@ -0,0 +1,8 @@
+-- leetcode 1581
+
+SELECT customer_id, COUNT(visit_id) AS count_no_trans
+FROM Visits
+WHERE visit_id NOT IN
+        (SELECT DISTINCT visit_id
+        FROM Transactions)
+GROUP BY customer_id;
