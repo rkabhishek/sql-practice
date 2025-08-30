120a7a0105b114bdccdb7865d04e064b0831a3e1
Abhishek Kaushik <akaushik079@gmail.com>
2025-08-30 14:10:44 +0530

    1581: Add solution which uses subquery

--- leetcode/customer-who-visited-but-did-not-transact.sql
+++ leetcode/customer-who-visited-but-did-not-transact.sql
@@ -1,9 +1,16 @@
 -- leetcode 1581
 
+-- solution 1 using left join
 SELECT customer_id, COUNT(*) AS count_no_trans
 FROM Visits v LEFT JOIN Transactions t
 ON v.visit_id = t.visit_id
 WHERE t.transaction_id IS NULL
+GROUP BY customer_id;
+
+-- solution 2 using subquery
+SELECT customer_id, COUNT(visit_id) AS count_no_trans
+FROM Visits
+WHERE visit_id NOT IN (SELECT DISTINCT visit_id FROM Transactions)
 GROUP BY customer_id;
 
 
