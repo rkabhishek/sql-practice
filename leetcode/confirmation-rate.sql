09e6a9fa825bb2879c353fbfb9cb8ad0841786a9
Abhishek Kaushik <akaushik079@gmail.com>
2025-09-03 20:05:18 +0530

    1934: Add solution

---
+++ leetcode/confirmation-rate.sql
@@ -0,0 +1,12 @@
+-- leetcode 1934
+
+WITH Confirmation_Rate
+AS (
+    SELECT user_id,
+        ROUND(COUNT(CASE WHEN action = 'confirmed' THEN 1 ELSE NULL END) / COUNT(*), 2) AS confirmation_rate
+    FROM Confirmations
+    GROUP BY user_id)
+
+SELECT s.user_id, COALESCE(cr.confirmation_rate, 0) AS confirmation_rate
+FROM Signups s LEFT JOIN Confirmation_Rate cr
+ON s.user_id = cr.user_id;
