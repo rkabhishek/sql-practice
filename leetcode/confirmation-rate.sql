97a1d1d1b59b2cc170dd61cb407de2b560cede56
Abhishek Kaushik <akaushik079@gmail.com>
2025-09-04 11:55:21 +0530

    1934: Add solution 2 that doesn't use CTE

--- leetcode/confirmation-rate.sql
+++ leetcode/confirmation-rate.sql
@@ -1,5 +1,6 @@
 -- leetcode 1934
 
+-- solution 1 using CTE and then LEFT JOIN
 WITH Confirmation_Rate
 AS (
     SELECT user_id,
@@ -10,3 +11,9 @@ AS (
 SELECT s.user_id, COALESCE(cr.confirmation_rate, 0) AS confirmation_rate
 FROM Signups s LEFT JOIN Confirmation_Rate cr
 ON s.user_id = cr.user_id;
+
+-- solution 2 using LEFT JOIN without CTE
+SELECT s.user_id, ROUND(AVG(CASE WHEN action = 'confirmed' THEN 1 ELSE 0 END), 2) AS confirmation_rate
+FROM Signups s LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
+GROUP BY s.user_id;
