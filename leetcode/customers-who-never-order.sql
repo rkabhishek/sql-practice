122f38dae500de672c028fade654eb69e7f67b5f
Abhishek Kaushik <akaushik079@gmail.com>
2025-09-09 13:01:11 +0530

    183: Add 2nd solution using NOT EXISTS

--- leetcode/customers-who-never-order.sql
+++ leetcode/customers-who-never-order.sql
@@ -1,6 +1,16 @@
 -- leetcode 183
 
+-- solution 1 using NOT IN
 SELECT name AS Customers
 FROM Customers WHERE id NOT IN (
     SELECT customerId FROM Orders
+);
+
+-- solution 2 using NOT EXISTS
+SELECT name AS Customers
+FROM Customers c
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM Orders o
+    WHERE o.customer_Id = c.id
 );
