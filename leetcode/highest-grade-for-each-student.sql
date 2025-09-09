1223d90c54a983c6888855c135707e1c1cc9b7fc
Abhishek Kaushik <akaushik079@gmail.com>
2025-09-09 15:05:14 +0530

    1112: Add solution with tuple comparison using IN

--- leetcode/highest-grade-for-each-student.sql
+++ leetcode/highest-grade-for-each-student.sql
@@ -1,5 +1,6 @@
 -- leetcode 1112
 
+-- solution 1 using JOIN
 SELECT e.student_id, MIN(e.course_id) AS course_id, e.grade
 FROM Enrollments e
 JOIN (
@@ -11,3 +12,14 @@ ON e.student_id = m.student_id
 WHERE e.grade = m.max_grade
 GROUP BY e.student_id
 ORDER BY e.student_id;
+
+-- solution 2 using IN
+SELECT student_id, MIN(course_id) AS course_id, grade
+FROM Enrollments
+WHERE (student_id, grade) IN (
+    SELECT student_id, MAX(grade)
+    FROM Enrollments
+    GROUP BY student_id
+)
+GROUP BY student_id
+ORDER BY student_id;
