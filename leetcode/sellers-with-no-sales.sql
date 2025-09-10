c234d3de61626e2eeb0c4fc7f80d3fd5ed277859
Abhishek Kaushik <akaushik079@gmail.com>
2025-09-10 08:09:43 +0530

    1607: Use YEAR() function to check if date satisfies condition

--- leetcode/sellers-with-no-sales.sql
+++ leetcode/sellers-with-no-sales.sql
@@ -6,7 +6,7 @@ FROM Orders o
 RIGHT JOIN Seller s
 ON o.seller_id = s.seller_id
 GROUP BY s.seller_id
-HAVING SUM(CASE WHEN sale_date BETWEEN '2020-01-01' AND '2020-12-31' THEN 1 ELSE 0 END) = 0
+HAVING SUM(CASE WHEN YEAR(sale_date) = 2020 THEN 1 ELSE 0 END) = 0
 ORDER BY s.seller_name;
 
 -- solution 2 using NOT EXISTS
@@ -16,6 +16,6 @@ WHERE NOT EXISTS (
     SELECT 1
     FROM Orders o
     WHERE s.seller_id = o.seller_id
-    AND sale_date BETWEEN '2020-01-01' AND '2020-12-31'
+    AND YEAR(sale_date) = 2020
 )
 ORDER BY s.seller_name;
