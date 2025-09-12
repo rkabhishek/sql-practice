028e0ff7129860847fb83ed4eab39b0f8dbbc39a
Abhishek Kaushik <akaushik079@gmail.com>
2025-09-12 11:39:03 +0530

    1571: Update solution to join only relevant columns from product table

--- leetcode/warehouse-manager.sql
+++ leetcode/warehouse-manager.sql
@@ -1,6 +1,11 @@
 -- leetcode 1571
 
-SELECT w.name AS warehouse_name, SUM(w.units * p.Width * p.Length * p.Height) AS volume
-FROM Warehouse w LEFT JOIN Products p
-ON w.product_id = p.product_id
+SELECT w.name AS warehouse_name, SUM(w.units * sub.product_volume) AS volume
+FROM Warehouse w LEFT JOIN (
+    SELECT
+        p.product_id,
+        p.width * p.length * p.height AS product_volume
+    FROM Products p
+) sub
+ON w.product_id = sub.product_id
 GROUP BY w.name;
