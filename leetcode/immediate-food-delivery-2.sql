44721b4f3e8c10d2f189997582454b913f643731
Abhishek Kaushik <akaushik079@gmail.com>
2025-09-12 15:18:11 +0530

    1174: Add solution using subquery + IN

--- leetcode/immediate-food-delivery-2.sql
+++ leetcode/immediate-food-delivery-2.sql
@@ -1,5 +1,6 @@
 -- leetcode 1174
 
+-- solution 1 using CTE + JOIN
 WITH First_Order AS (
     SELECT customer_id, MIN(ORDER_date) AS order_date
     FROM Delivery
@@ -9,3 +10,14 @@ WITH First_Order AS (
 SELECT ROUND(COUNT(CASE WHEN d.customer_pref_delivery_date = d.order_date THEN 1 END) * 100 / COUNT(*), 2) AS immediate_percentage
 FROM Delivery d JOIN First_Order f
 ON d.customer_id = f.customer_id AND d.order_date = f.order_date;
+
+-- solution 2 using subquery and IN
+
+SELECT ROUND(AVG(order_date = customer_pref_delivery_date) * 100.0, 2) AS immediate_percentage
+FROM Delivery
+WHERE (customer_id, order_date)
+IN (
+    SELECT customer_id, MIN(order_date)
+    FROM Delivery
+    GROUP BY customer_id
+);
