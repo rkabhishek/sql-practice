0fbbf7a4aac7c5b209e962c2b119efeaef32a47e
Abhishek Kaushik <akaushik079@gmail.com>
2025-09-12 12:03:27 +0530

    586: Add solution which works for any number of customers placing max orders

--- leetcode/customer-placing-largest-number-of-orders.sql
+++ leetcode/customer-placing-largest-number-of-orders.sql
@@ -1,7 +1,18 @@
 -- leetcode 586
 
+-- solution 1 which works for 1 customer with max orders
 SELECT customer_number
 FROM Orders
 GROUP BY customer_number
 ORDER BY COUNT(order_number) DESC
 LIMIT 1;
+
+-- solution 2 which works for any number of customers placing max orders
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+HAVING COUNT(order_number) >= ALL(
+    SELECT COUNT(order_number)
+    FROM Orders
+    GROUP BY customer_number
+);
