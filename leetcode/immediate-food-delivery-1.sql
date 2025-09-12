a36596699c5ec4b047abf97f0fa0f31f914d9800
Abhishek Kaushik <akaushik079@gmail.com>
2025-09-12 14:20:53 +0530

    1173: Add solution using SUM

--- leetcode/immediate-food-delivery-1.sql
+++ leetcode/immediate-food-delivery-1.sql
@@ -1,4 +1,9 @@
 -- leetcode 1173
 
+-- solution 1: using CASE works in every SQL engine
 SELECT ROUND(COUNT(CASE WHEN order_date = customer_pref_delivery_date THEN 1 END) * 100.0 / COUNT(*), 2) AS immediate_percentage
+FROM Delivery;
+
+-- solution 2: may not work in every SQL engine because boolean isn't converted to 0 and 1
+SELECT ROUND(SUM(order_date = customer_pref_delivery_date) * 100.0 / COUNT(*), 2) AS immediate_percentage
 FROM Delivery;
