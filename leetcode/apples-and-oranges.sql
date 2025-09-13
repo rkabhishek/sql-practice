5a8a437db6babf9e2b2e697ae9bb4bfd35d2f15f
Abhishek Kaushik <akaushik079@gmail.com>
2025-09-13 12:19:18 +0530

    1445: Add solution using Self-join

--- leetcode/apples-and-oranges.sql
+++ leetcode/apples-and-oranges.sql
@@ -1,5 +1,11 @@
 -- leetcode 1445
 
+-- solution 1 using SUM and CASE
 SELECT sale_date, SUM(CASE WHEN fruit = 'apples' THEN sold_num ELSE -sold_num END) AS diff
 FROM Sales
 GROUP BY sale_date;
+
+-- solution 2 using self-join
+SELECT a.sale_date, a.sold_num - b.sold_num AS diff
+FROM sales a JOIN sales b
+ON a.sale_date = b.sale_date AND a.fruit = 'apples' AND b.fruit = 'oranges';
