c6ad2d87c9b4fd7e7860e344e36cd488bd774c3a
Abhishek Kaushik <akaushik079@gmail.com>
2025-09-16 14:09:58 +0530

    1303: Add solution

--- leetcode/find-the-team-size.sql
+++ leetcode/find-the-team-size.sql
@@ -1,5 +1,6 @@
 -- leetcode 1303
 
+-- solution 1 using CTE and JOIN
 WITH Team_Size AS (
     SELECT team_id, COUNT(*) AS team_size
     FROM Employee
@@ -10,3 +11,8 @@ SELECT e.employee_id, t.team_size
 FROM Employee e
 JOIN Team_Size t
 ON e.team_id = t.team_id;
+
+-- solution 2 using window function
+SELECT employee_id,
+       COUNT(*) OVER (PARTITION BY team_id) AS team_size
+FROM Employee;
