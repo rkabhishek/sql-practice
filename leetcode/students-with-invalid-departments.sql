b10d1b1e10a4875a3d2ade3cd97d6ebc841adc01
Abhishek Kaushik <akaushik079@gmail.com>
2025-09-16 11:47:16 +0530

    1350: Add solution using LEFT JOIN

--- leetcode/students-with-invalid-departments.sql
+++ leetcode/students-with-invalid-departments.sql
@@ -1,5 +1,13 @@
 -- leetcode 1350
 
+-- solution 1 using subquery and NOT IN
 SELECT id, name
 FROM Students
 WHERE department_id NOT IN (SELECT id FROM Departments);
+
+-- solution 2 using LEFT JOIN
+SELECT s.id, s.name
+FROM Students s
+LEFT JOIN Departments d
+ON s.department_id = d.id
+WHERE d.id IS NULL;
