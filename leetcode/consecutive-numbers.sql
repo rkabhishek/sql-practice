ea3b63af62e919aef2825bc0edf96716133601b8
Abhishek Kaushik <akaushik079@gmail.com>
2025-09-18 13:16:07 +0530

    180: Add solution using JOIN

--- leetcode/consecutive-numbers.sql
+++ leetcode/consecutive-numbers.sql
@@ -1,5 +1,6 @@
 -- leetcode 180
 
+-- solution 1 using LEAD window function
 SELECT DISTINCT num AS ConsecutiveNums
 FROM (
     SELECT id,
@@ -9,3 +10,11 @@ FROM (
     FROM Logs
 ) t
 WHERE num = next1 and num = next2;
+
+-- solution 2 using JOIN
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs l1
+JOIN Logs l2 ON l1.id = l2.id - 1
+JOIN Logs l3 ON l1.id = l3.id - 2
+WHERE l1.num = l2.num
+AND l2.num = l3.num;
