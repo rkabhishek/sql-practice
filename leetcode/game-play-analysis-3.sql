cbb765747295d23ec8a00a2148c6e09ac9a4e162
Abhishek Kaushik <akaushik079@gmail.com>
2025-09-20 14:23:24 +0530

    534: Add solution using SUM() OVER

--- leetcode/game-play-analysis-3.sql
+++ leetcode/game-play-analysis-3.sql
@@ -1,5 +1,6 @@
 -- leetcode 534
 
+-- solution 1 using self-join
 SELECT a1.player_id, a1.event_date, SUM(a2.games_played) AS games_played_so_far
 FROM Activity a1
 JOIN (
@@ -8,3 +9,13 @@ JOIN (
 ) a2
 ON a1.player_id = a2.player_id AND a1.event_date >= a2.event_date
 GROUP BY a1.player_id, a1.event_date;
+
+-- solution 2 using SUM() OVER
+SELECT
+    player_id, event_date,
+    SUM(games_played) OVER (
+        PARTITION BY player_id
+        ORDER BY event_date
+        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+    ) AS games_played_so_far
+FROM Activity;
