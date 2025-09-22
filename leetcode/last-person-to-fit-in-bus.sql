cb14140b86d9848540449493973ec6e2735b588d
Abhishek Kaushik <akaushik079@gmail.com>
2025-09-22 14:38:28 +0530

    1204: Optimise solution by not scanning the CTE twice

--- leetcode/last-person-to-fit-in-bus.sql
+++ leetcode/last-person-to-fit-in-bus.sql
@@ -1,11 +1,16 @@
 -- leetcode 1204
 
 WITH Queue_With_Cumulative_Weight AS(
-    SELECT turn, person_id, person_name, weight,
+    SELECT person_name,
         SUM(weight) OVER (ORDER BY turn ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS cumulative_weight
     FROM Queue
 )
 
 SELECT person_name
-FROM Queue_With_Cumulative_Weight
-WHERE cumulative_weight = (SELECT MAX(cumulative_weight) FROM Queue_With_Cumulative_Weight WHERE cumulative_weight <= 1000);
+FROM (
+    SELECT person_name,
+        ROW_NUMBER() OVER (ORDER BY cumulative_weight DESC) AS row_num
+    FROM Queue_With_Cumulative_Weight
+    WHERE cumulative_weight <= 1000
+) t
+WHERE t.row_num = 1;
