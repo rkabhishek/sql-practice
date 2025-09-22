c5399c14378120ae8deb465860b3da87e0d85799
Abhishek Kaushik <akaushik079@gmail.com>
2025-09-22 22:59:16 +0530

    1907: Add solution using SUM and CASE

--- leetcode/count-salary-categories.sql
+++ leetcode/count-salary-categories.sql
@@ -1,5 +1,6 @@
 -- leetcode 1907
 
+-- solution 1 using CTE
 WITH Categories AS (
     SELECT 'High Salary' AS category
     UNION ALL
@@ -22,3 +23,20 @@ SELECT a.category, COUNT(b.category) AS accounts_count
 FROM Categories a
 LEFT JOIN Categorization b ON a.category = b.category
 GROUP BY a.category;
+
+-- solution 2 using SUM and CASE
+SELECT 'Low Salary' AS category,
+    SUM(CASE WHEN INCOME < 20000 THEN 1 ELSE 0 END) AS accounts_count
+FROM Accounts
+
+UNION ALL
+
+SELECT 'Average Salary' AS category,
+    SUM(CASE WHEN INCOME BETWEEN 20000 AND 50000 THEN 1 ELSE 0 END) AS accounts_count
+FROM Accounts
+
+UNION ALL
+
+SELECT 'High Salary' AS category,
+    SUM(CASE WHEN INCOME > 50000 THEN 1 ELSE 0 END) AS accounts_count
+FROM Accounts;
