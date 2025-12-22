89c4bd2226007f7959539b7b9559b6ecbbbabbf2
Abhishek Kaushik <akaushik079@gmail.com>
2025-12-22 21:44:32 +0530

    1667: Replace LEFT() with SUBSTRING() for portability

--- leetcode/fix-names-in-table.sql
+++ leetcode/fix-names-in-table.sql
@@ -1,5 +1,5 @@
 -- leetcode 1667
 
-SELECT user_id, CONCAT(UPPER(LEFT(name, 1)), LOWER(SUBSTRING(name, 2))) AS name
+SELECT user_id, CONCAT(UPPER(SUBSTRING(name, 1, 1)), LOWER(SUBSTRING(name, 2))) AS name
 FROM Users
 ORDER BY user_id;
