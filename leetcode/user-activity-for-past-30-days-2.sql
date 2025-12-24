5be525a14659a7ac3222064b9ecf63fa0a676ed9
Abhishek Kaushik <akaushik079@gmail.com>
2025-12-24 14:15:18 +0530

    1142: Refactor using CTE for readability

--- leetcode/user-activity-for-past-30-days-2.sql
+++ leetcode/user-activity-for-past-30-days-2.sql
@@ -1,10 +1,16 @@
 -- leetcode 1142
 
-SELECT COALESCE(
-           ROUND(
-               1.0 * COUNT(DISTINCT session_id)
-               / NULLIF(COUNT(DISTINCT user_id), 0),
-           2),
-       0) AS average_sessions_per_user
-FROM Activity
-WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27';
+-- Steps:
+-- 1. Aggregate first (CTE) and divide safely using NULLIF
+-- 2. Round the result to 2 decimal places
+-- 3. Use COALESCE to result in 0 in case calculation returns NULL
+
+WITH User_Session_Info AS (
+    SELECT COUNT(DISTINCT session_id) AS sessions, NULLIF(COUNT(DISTINCT user_id), 0) AS users
+    FROM Activity
+    WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+)
+
+
+SELECT COALESCE(ROUND(1.0 * sessions / users, 2), 0) AS average_sessions_per_user
+FROM User_Session_Info;
