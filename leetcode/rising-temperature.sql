84a4c9e64246551133c8019dee64a27660331ff5
Abhishek Kaushik <akaushik079@gmail.com>
2025-12-26 20:19:06 +0530

    LC-197: refactor by putting the conditions in the JOIN logic instead of first doing a cross join and then filtering using WHERE

--- leetcode/rising-temperature.sql
+++ leetcode/rising-temperature.sql
@@ -1,6 +1,7 @@
 -- leetcode 197
 
 SELECT w1.id
-FROM Weather w1 JOIN Weather w2
-WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1
+FROM Weather w1
+JOIN Weather w2
+ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
 AND w1.temperature > w2.temperature;
