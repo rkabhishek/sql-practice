978a16f6a6a69773c71eb6e28dcb613c9ac69e52
Abhishek Kaushik <akaushik079@gmail.com>
2025-12-29 23:31:50 +0530

    LC-196: add solution using subquery

--- leetcode/delete-duplicate-emails.sql
+++ leetcode/delete-duplicate-emails.sql
@@ -1,5 +1,6 @@
 -- leetcode 196
 
+-- solution 1 using JOIN
 WITH Smallest_Id AS (
     SELECT MIN(id) AS id
     FROM Person
@@ -11,3 +12,13 @@ FROM Person p
 LEFT JOIN Smallest_Id s
 ON p.id = s.id
 WHERE s.id IS NULL;
+
+-- solution 2 using subquery
+WITH Min_Id AS (
+    SELECT MIN(id) AS id
+    FROM Person
+    GROUP BY email
+)
+
+DELETE FROM Person
+WHERE id NOT IN (SELECT id FROM Min_Id)
